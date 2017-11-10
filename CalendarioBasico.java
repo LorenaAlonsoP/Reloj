ab3a9fc543c7f36661c651cd27dcea73951884a1
Lorena Alonso <lorenaalonsop26@gmail.com>
2017-11-10 13:27:15 +0100

    Nuevas clases añadidas

---
+++ CalendarioBasico.java
@@ -0,0 +1,56 @@
+/**
+ * Write a description of class CalendarioBasico here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CalendarioBasico
+{
+    // Declarando campos di
+    private DisplayDosCaracteres day;
+    private DisplayDosCaracteres month;
+    private DisplayDosCaracteres year;
+
+    /**
+     * Constructor para calendario basico.
+     */
+    public CalendarioBasico()
+    {
+        day = new DisplayDosCaracteres(31);
+        month = new DisplayDosCaracteres(13);
+        year = new DisplayDosCaracteres(2100);
+    }
+
+    /**
+     * Permite introducir el dia, mes y año de una fecha.
+     */
+    public void fijarFecha(int dia, int mes, int ano)
+    {
+        day.setValorAlmacenado(dia);
+        month.setValorAlmacenado(mes);
+        year.setValorAlmacenado(ano);
+    }
+
+    /**
+     * Nos permite avanzar dia a dia en la fecha introducida.
+     */
+    public void avanzarFecha()
+    {
+        day.incrementaValorAlmacenado();
+        if (day.getValorAlmacenado() == 1) {
+            month.incrementaValorAlmacenado();
+            if (month.getValorAlmacenado() == 1) {
+                year.incrementaValorAlmacenado();
+            }
+        }
+    }
+
+    /**
+     * Permite devolver una fecha.
+     */
+    public String obtenerFecha()
+    {
+        return day.getTextoDelDisplay() + "-" + month.getTextoDelDisplay() + "-" + year.getTextoDelDisplay();
+    }
+
+}
---
+++ FechaYHora.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class FechaYHora here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FechaYHora
+{
+    // instance variables - replace the example below with your own
+    private CalendarioBasico fecha;
+    private ClockDisplay hora;
+
+    /**
+     * Constructor for objects of class FechaYHora
+     */
+    public FechaYHora()
+    {
+        fecha = new CalendarioBasico ();
+        hora = new ClockDisplay ();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getFechaYHora(int y)
+    {
+        // put your code here
+        return
+    }
+}
